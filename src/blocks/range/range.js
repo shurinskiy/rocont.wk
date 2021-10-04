6de751dde7c5bcc30347d169743fb0f50f8a4a53
_tango <shurinskiy@mail.ru>
2021-10-04 20:24:24 +0600

    calculator sliders numeral

--- src/blocks/range/range.js
+++ src/blocks/range/range.js
@@ -5,6 +5,7 @@
 		let min = $(this).attr('min') || 0;
 		let max = $(this).attr('max') || 100;
 		let round = 10**(min.length-1);
+		let numeral = $self.data("numeral");
 
 		let getPercentage = (value) => {
 			value = value || 0;
@@ -12,6 +13,19 @@
 		}
 
 		let getRound = (a) => Math.round(a / round) * round;
+
+		let declOfNum = (n, data) => { 
+			if (!data) { return '' };
+
+			let text_forms = data.split(",");
+			n = Math.abs(n) % 100; 
+			let n1 = n % 10;
+
+			if (n > 10 && n < 20) { return text_forms[2].trim(); }
+			if (n1 > 1 && n1 < 5) { return text_forms[1].trim(); }
+			if (n1 == 1) { return text_forms[0].trim(); }
+			return text_forms[2].trim();
+		}
 
 		$self
 			.addClass('range__input')
@@ -29,15 +43,14 @@
 
 		$scale.css({width: initial});
 		$handler.css({left: initial});
-		$output.text($self.val());
+		$output.text(`${$self.val()} ${declOfNum($self.val(), numeral)}`);
 		
 		$self.on('change input', function(e) {
 			let value = getPercentage(this.value);
 
 			$scale.css({width: value});
 			$handler.css({left: value});
-			$output.text(getRound(this.value));
+			$output.text(`${getRound(this.value)} ${declOfNum(this.value, numeral)}`);
 		});
 	});
-
 })();
